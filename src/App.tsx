153cbf53daf93449e8e98932c52fcf2069f4b3ea
salman3xs <salman3xs@gmial.com>
2023-02-09 23:29:54 +0530

    react init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import Navbar from "./pages/navbar";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/example.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from "react";
+
+const Example : FC = (): JSX.Element =>{
+    return(
+        <div>
+            
+        </div>
+    )
+}
+export default Example;
